e821256152c2cea234258757a8a6f7e5023e1748
Alex Ross <alex@enplug.com>
2015-12-15 20:32:04 -0800

    Add test framework

---
+++ grunt/karma.js
@@ -0,0 +1,23 @@
+module.exports = {
+    options: {
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'dist/sdk.js',
+            'test/*.js',
+        ],
+        frameworks: ['jasmine'],
+        browsers: ['PhantomJS'],
+    },
+    unit: {
+        singleRun: true,
+        preprocessors: {
+            'dist/sdk.js': 'coverage'
+        },
+        reporters: ['coverage'],
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/',
+        },
+    },
+};
