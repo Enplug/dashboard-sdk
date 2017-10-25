ba423421282ab67e9c16aa037116af2db07d5d5c
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-10-25 13:41:27 +0200

    fix formatting

--- src/social.js
+++ src/social.js
@@ -1,34 +1,32 @@
 ;(function (enplug, document) {
-  'use strict';
+    'use strict';
 
-  /**
-   * Social feeds functionality
-   *
-   *
-   * @class
-   * @extends Sender
-   */
-  function SocialSender () {
+    /**
+     * Social feeds functionality
+     *
+     * @class
+     * @extends Sender
+     */
+    function SocialSender() {
 
-    // Call parent constructor with namespace
-    enplug.classes.Sender.call(this, 'social');
+        // Call parent constructor with namespace
+        enplug.classes.Sender.call(this, 'social');
 
-    this.ttest = function (msg, onSuccess, onError) {
-        this.validate(msg, 'string', 'No message provided.');
-        return this.method({
-            name: 'ttest',
-            params: msg,
-            transient: true,
-            successCallback: onSuccess,
-            errorCallback: onError
-        });
+        this.ttest = function (msg, onSuccess, onError) {
+            this.validate(msg, 'string', 'No message provided.');
+            return this.method({
+                name: 'ttest',
+                params: msg,
+                successCallback: onSuccess,
+                errorCallback: onError
+            });
+        }
     }
-  }
 
-  // Inherit
-  SocialSender.prototype = Object.create(enplug.classes.Sender.prototype);
+    // Inherit
+    SocialSender.prototype = Object.create(enplug.classes.Sender.prototype);
 
-  // Export
-  enplug.classes.SocialSender = SocialSender;
-  enplug.social = new SocialSender();
+    // Export
+    enplug.classes.SocialSender = SocialSender;
+    enplug.social = new SocialSender();
 }(window.enplug, document));
